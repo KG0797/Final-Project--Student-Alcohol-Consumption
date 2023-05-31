5fab05331dc78aff0badb6c5bd6e7275c83ddf58
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-05-31 12:16:38 -0700

    Update 10queries.py

--- 10queries.py
+++ 10queries.py
@@ -1,3 +1,8 @@
 Katherine Guo-
 9. CREATE TABLE higher_eudcation AS SELECT id, schoolsup, famsup, paid, higher, G3 FROM student_new;
 10. CREATE TABLE health_condition AS SELECT age, sex, Dalc, Walc, health FROM student_new;
+
+Katherine Wei
+CREATE TABLE failures AS SELECT Dalc, Walc, studytime, failures, COUNT(*) AS count FROM student_new GROUP BY failures;
+CREATE TABLE purpose AS SELECT studytime, failures, higher, COUNT(*) AS count FROM student_new GROUP BY studytime, failures, higher;
+
