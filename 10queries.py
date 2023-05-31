8ca874573b9f2dc27b5f610df95756c554c85fe8
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-05-31 11:08:03 -0700

    Update 10queries.py

--- 10queries.py
+++ 10queries.py
@@ -1,2 +1,3 @@
 Katherine Guo-
 9. CREATE TABLE higher_eudcation AS SELECT id, schoolsup, famsup, paid, higher, G3 FROM student_new;
+10. CREATE TABLE health_condition AS SELECT age, sex, Dalc, Walc, health FROM student_new;
