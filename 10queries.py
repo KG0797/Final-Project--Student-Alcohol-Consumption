5667611f6a4c4bdd98d2b04d55c92df65a71f6ad
bonche22 <121894893+bonche22@users.noreply.github.com>
2023-06-03 10:43:20 -0700

    Update 10queries.py

--- 10queries.py
+++ 10queries.py
@@ -34,66 +34,64 @@ CREATE TABLE student_new (
     G2 TEXT,
     G3 TEXT);
 
-
 --Yingchen Yang
+--Will the frequency of going out with friends affect alcohol consumption
 CREATE TABLE Goout_alcohol (
   id TEXT PRIMARY KEY,
   Dalc TEXT,
   Walc TEXT,
   goout TEXT
 );
-
 INSERT INTO Goout_alcohol (id, Dalc, Walc, goout)
 SELECT id, Dalc, Walc, goout
 FROM student_new;
 
+--Will internet affect the consumption of alcohol
 CREATE TABLE Internet_alcohol (
   id TEXT PRIMARY KEY,
   Dalc TEXT,
   Walc TEXT,
   internet TEXT
 );
-
 INSERT INTO Internet_alcohol (id, Dalc, Walc, internet)
 SELECT id, Dalc, Walc, internet
 FROM student_new;
 
 --Katherine Wei
+--How daily and weekend alcohol consumption affect a students’ student time and how it reflect the number of past classes failure 
 CREATE TABLE failures AS SELECT Dalc, Walc, studytime, failures, COUNT(*) AS count FROM student_new GROUP BY failures;
+--How does alcohol consumption affect students’ performance in school and studying, giving more enthusiasm or not?
 CREATE TABLE purpose AS SELECT studytime, failures, higher, COUNT(*) AS count FROM student_new GROUP BY studytime, failures, higher;
 
 --Ziyu Lin
+--If family living condition have relationship with alcohol consumption
+--Why choose this schoo
 INSERT INTO student_new SELECT * FROM student_por;
-
 CREATE TABLE schoolreason AS SELECT school, sex, reason, COUNT(*) as count
 FROM student_new GROUP BY school, sex, reason;
-
 CREATE TABLE familyliving_temp AS Select DISTINCT id, school, sex, age, 
 address, famsize, Pstatus, Famrel, Dalc, Walc From student_new;
-
 CREATE TABLE familyliving AS SELECT *FROM familyliving_temp ORDER BY Dalc DESC, Walc DESC;
 
-
--- Bonnie
-  -- How does alcohol consumption affect students’ Math or Portuguese grade over time
-  SELECT Dalc, Walc, AVG(G1) AS Average_Grade_G1, AVG(G2) AS Average_Grade_G2, AVG(G3) AS Average_Grade_G3
-  FROM student_new
-  GROUP BY Dalc, Walc
-  ORDER BY Dalc, Walc;
-  -- How does mom's jobs and guardian related to alcohol consumption?
-  SELECT Mjob, guardian, AVG(Dalc) AS Average_Daily_Alcohol, AVG(Walc) AS Average_Weekly_Alcohol
-  FROM student_new
-  GROUP BY Mjob, guardian
-  ORDER BY Mjob, guardian;
-  -- How does dad's jobs and guardian related to alcohol consumption?
-  SELECT Fjob, guardian, AVG(Dalc) AS Average_Daily_Alcohol, AVG(Walc) AS Average_Weekly_Alcohol
-  FROM student_new
-  GROUP BY Fjob, guardian
-  ORDER BY Fjob, guardian;
-
+-- Bonnie Chen
+-- How does alcohol consumption affect students’ Math or Portuguese grade over time
+SELECT Dalc, Walc, AVG(G1) AS Average_Grade_G1, AVG(G2) AS Average_Grade_G2, AVG(G3) AS Average_Grade_G3
+FROM student_new
+GROUP BY Dalc, Walc
+ORDER BY Dalc, Walc;
+-- How does mom's jobs and guardian related to alcohol consumption?
+SELECT Mjob, guardian, AVG(Dalc) AS Average_Daily_Alcohol, AVG(Walc) AS Average_Weekly_Alcohol
+FROM student_new
+GROUP BY Mjob, guardian
+ORDER BY Mjob, guardian;
+-- How does dad's jobs and guardian related to alcohol consumption?
+SELECT Fjob, guardian, AVG(Dalc) AS Average_Daily_Alcohol, AVG(Walc) AS Average_Weekly_Alcohol
+FROM student_new
+GROUP BY Fjob, guardian
+ORDER BY Fjob, guardian;
 
 --Katherine Guo
+--Higher grade means they like to take higher education
 CREATE TABLE higher_eudcation AS SELECT id, schoolsup, famsup, paid, higher, G3 FROM student_new;
+--Health condition for different age and sex students of alcohol consumption
 CREATE TABLE health_condition AS SELECT age, sex, Dalc, Walc, health FROM student_new;
-
-
