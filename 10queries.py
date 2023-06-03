9b664129bf6e630e47199955b1d3b93b1d3f4f23
Yingchen-Yang <129204557+Yingchen-Yang@users.noreply.github.com>
2023-06-03 10:16:07 -0700

    Update 10queries.py

--- 10queries.py
+++ 10queries.py
@@ -1,7 +1,29 @@
+--Yingchen Yang
+CREATE TABLE Goout_alcohol (
+  id TEXT PRIMARY KEY,
+  Dalc TEXT,
+  Walc TEXT,
+  goout TEXT
+);
 
-Katherine Wei
-3. CREATE TABLE failures AS SELECT Dalc, Walc, studytime, failures, COUNT(*) AS count FROM student_new GROUP BY failures;
-4. CREATE TABLE purpose AS SELECT studytime, failures, higher, COUNT(*) AS count FROM student_new GROUP BY studytime, failures, higher;
+INSERT INTO Goout_alcohol (id, Dalc, Walc, goout)
+SELECT id, Dalc, Walc, goout
+FROM student_por;
+
+CREATE TABLE Internet_alcohol (
+  id TEXT PRIMARY KEY,
+  Dalc TEXT,
+  Walc TEXT,
+  internet TEXT
+);
+
+INSERT INTO Internet_alcohol (id, Dalc, Walc, internet)
+SELECT id, Dalc, Walc, internet
+FROM student_por;
+
+--Katherine Wei
+CREATE TABLE failures AS SELECT Dalc, Walc, studytime, failures, COUNT(*) AS count FROM student_new GROUP BY failures;
+CREATE TABLE purpose AS SELECT studytime, failures, higher, COUNT(*) AS count FROM student_new GROUP BY studytime, failures, higher;
 
 
 
@@ -9,8 +31,8 @@ Katherine Wei
 
 
 
-Katherine Guo-
-9. CREATE TABLE higher_eudcation AS SELECT id, schoolsup, famsup, paid, higher, G3 FROM student_new;
-10. CREATE TABLE health_condition AS SELECT age, sex, Dalc, Walc, health FROM student_new;
+--Katherine Guo
+CREATE TABLE higher_eudcation AS SELECT id, schoolsup, famsup, paid, higher, G3 FROM student_new;
+CREATE TABLE health_condition AS SELECT age, sex, Dalc, Walc, health FROM student_new;
 
 
